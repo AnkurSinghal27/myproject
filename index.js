ab0594828fc1c41ffd6f18c138644f5c1fe1ed43
AnkurSinghal27 <41937633+AnkurSinghal27@users.noreply.github.com>
2022-03-01 22:30:41 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// console.log("hello world");
+// //alert("me");
+// // document.write("dc")
+// console.warn("this is warning")
+// console.error("this is an error")
+// console.clear()
+// j. javasript variables
+//contanier to store data values
+var number1 = 34;
+var number2 = 56;
+//console.log(number1 + number2);
+
+
+//data type in java Script
+var num1 = 455;
+var num2 = 56.76;
+//string
+var str1 = "this is a string";
+var str2 = 'this is also a string'
+//objects
+var marks = {
+    ravi: 34,
+    shubham: 78,
+    harry: 99.977
+}
+
+// console.log(marks)
+//boolens
+var a= true;
+var b= false;
+// console.log(a,b);
+
+var und = undefined;
+// console.log(undefined);
+// console.log(und);
+var und;
+// console.log(und)
+var n = null;
+// console.log(n);
+
+/*two type of data types
+1.primitive datatype; undefine,null,number,string,boolean,symbol
+2.reference data typeof;arrays and objects
+
+Symbol("")
+*/
+var arr = [1,2,3,4,5]
+var arr = [1,2,"bablu",4,5]
+// console.log(arr);
+// operatos in java scripts
+var a = 36;
+var b = 56;
+console.log("the value of a+b is", a+b);
+console.log("the value of a-b is", a-b);
+console.log("the value of a/b is", a/b);
+console.log("the value of a*b is", a*b);
+
+// assinment operatot
+var c = b;
+c+=b;
+c-=b;
+c*=b;
+c/=b;
+console.log(c);
+// comparision operator
+var x= 34;
+var y= 56;
+console.log(x==y);
+console.log(x<=y);
+console.log(x>=y);
+
+console.log(true&&true);
+console.log(true&&false);
+
+
+
+
+
